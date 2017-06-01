e3821ae76ed6823b234c90c18e1fb8e966a40347
zouyang@baokeyun.com <86022310@qq.com>
2017-06-01 23:34:07 +0800

    m-header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  export default {
+    components: {
+      MHeader
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-
-  #app {
-    color: $color-theme
-  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+// 推荐页面
+import Recommend from 'components/recommend/recommend'
+// 歌手页面
+import Singer from 'components/singer/singer'
+// 排行页面
+import Rank from 'components/rank/rank'
+// 搜索页面
+import Search from 'components/search/search'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/Search',
+      component: search
     }
   ]
 })
