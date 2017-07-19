24f1701ea2f6a323bc6e5ea238ba4f7a4c2f155d
zouyang@baokeyun.com <86022310@qq.com>
2017-07-19 22:26:07 +0800

    5-4

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,13 +1,38 @@
 <template>
-
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="group in data" class="list-group">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" class="list-group-item">
+            <img v-lazy="item.avatar" class="avatar"/>
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props() {
-      return {
+  import Scroll from 'base/scroll/scroll'
 
+  export default {
+    props: {
+      data: {
+        type: Array,
+        default: []
       }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.title.map((group) => {
+          return group.title.substr(0, 1)
+        })
+      }
+    },
+    components: {
+      Scroll
     }
   }
 </script>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-    歌手页面
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
 
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10
@@ -25,8 +26,7 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            this.singers = res.data.list
-            this._normalizeSinger(this.singers)
+            this.singers = this._normalizeSinger(res.data.list)
           }
         })
       },
@@ -40,7 +40,7 @@
         list.forEach((item, index) => {
           if (index < HOT_SINGER_LEN) {
             map.hot.items.push(new Singer({
-              id: item.Fsinger_id,
+              id: item.Fsinger_mid,
               name: item.Fsinger_name
             }))
           }
@@ -52,7 +52,7 @@
             }
           }
           map[key].items.push(new Singer({
-            id: item.Fsinger_id,
+            id: item.Fsinger_mid,
             name: item.Fsinger_name
           }))
         })
@@ -72,6 +72,9 @@
         })
         return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
