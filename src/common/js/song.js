8a26580d650698099424cc42dfd9fc491852bc14
zouyang <zouyang@baokeyun.com>
2017-11-07 17:23:08 +0800

    6-7

---
+++ src/common/js/song.js
@@ -0,0 +1,41 @@
+// 不设置成对像，设置成类有2大好处
+// 1.可以把代集中到一个地方去维护，不需要在多个地方去大量的写这种代码
+// 2.类的扩展性比对像要强很多，而且他是一种面向对象的编程方式
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image, url}) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+    this.url = url
+  }
+}
+
+// 工厂方法：不直接去new,提供一个方法去,这样就不用在代码里写很多new
+export function createSong(musicData) {
+  return new Song({
+    id: musicData.songid,  // songid必须存在
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,  // albummid必须存在
+    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+  })
+}
+
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
+  })
+  return ret.join('/')
+}
+
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,35 +1,70 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
+  import {ERR_OK} from 'api/config'
+  import {getSingerDetail} from 'api/singer'
+  import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
     },
     created() {
-      console.log(this.singer)
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        // 当在singer-detail页面不小心刷新了，singer.id不存在，就让其跳回singer页面
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          // 对象解构赋值
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active,.slide-leave-active
     transition: all .3s ease
